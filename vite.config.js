475780628d15a9e3001805e41b0f1b5a68456fc8
micor <micor.cjj@gmail.com>
2025-06-24 01:31:27 +0800

    change

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,35 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({ autoImport: true }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  // 1. 读取 .env 文件变量
+  const env = loadEnv(mode, process.cwd())
+  // 2. 读取前缀，自动生成 proxy 配置
+  const prefixes = (env.VITE_PROXY_PREFIXES || '').split(',').map(i => i.trim()).filter(Boolean)
+
+  const proxy = {}
+  prefixes.forEach(prefix => {
+    proxy[prefix] = {
+      target: env.VITE_PROXY_TARGET,
+      changeOrigin: true,
+      rewrite: path => path.replace(new RegExp('^' + prefix), prefix),
+    }
+  })
+
+  return {
+    plugins: [
+      vue(),
+      vuetify({ autoImport: true }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      }
+    server: {
+      proxy,
     }
   }
 })
